b0e47b6ff26f494e1c9763b24c1a7c9b490e9138
Omanshu Aman <omanshuaman@gmail.com>
2022-08-07 12:04:51 +0530

    10. WhatsApp Clone -Design Chat Activity and Move to Chat Activity With Data

---
+++ app/src/main/java/com/example/bdchat/ChatDetailActivity.java
@@ -0,0 +1,44 @@
+package com.example.bdchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bdchat.databinding.ActivityChatDetailBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        final String senderId = auth.getUid();
+        String recieveId = getIntent().getStringExtra("userId");
+        String userName = getIntent().getStringExtra("userName");
+        String profilePic = getIntent().getStringExtra("ProfilePic");
+
+        binding.userName.setText(userName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.avatar).into(binding.profileImage);
+
+        binding.backArrow.setOnClickListener(v -> {
+            Intent intent = new Intent(ChatDetailActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+
+}
